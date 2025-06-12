210f7095384bfee899d73a39132556c7dd8c5ed4
arnav260713 <x15amar@iima.ac.in>
2025-06-12 17:46:40 +0530

    commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+print("helllo world")
+a=10
+print(type(a))
+b=9.879
+print(type(b))
+c="arnav"
+print(type(c))
+d=True
+print(type(d))
+
+#typecasting
+print(type(float(a)))
+s=str(a)
+print(type(s))
+
+#stringopration
+name="codingal"
+print(name[::-1])
+print(name.upper())
