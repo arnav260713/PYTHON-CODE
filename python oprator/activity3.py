aa95175a82d16cf8fd8ced3dd9f6f0642d5cb5a4
arnav260713 <x15amar@iima.ac.in>
2025-06-14 14:12:31 +0530

    commit

---
+++ python oprator/activity3.py
@@ -0,0 +1,7 @@
+marks1=int(input("enter your maths marks-"))
+marks2=int(input("enter your hindi marks-"))
+marks3=int(input("enter your english marks-"))
+marks4=int(input("enter your science marks-"))
+marks5=int(input("enter your sst marks-"))
+percentage=((marks1+marks2+marks3+marks4+marks5)/500)*100
+print("percentage of all marks-",percentage)
