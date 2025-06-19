9784b15581d6a18d10358490017a3b5f6e8f9c94
arnav260713 <x15amar@iima.ac.in>
2025-06-19 19:50:01 +0530

    commit

---
+++ extra project/storingbirthday.py
@@ -0,0 +1,11 @@
+pulkitbirthday="22 march "
+kavyabirthday="13 apirl"
+tarabirthday="26 june"
+anantbirthday="21 june"
+kukubirthday="26 july"
+print("birthday date")
+print(pulkitbirthday)
+print(kavyabirthday)
+print(tarabirthday)
+print(anantbirthday)
+print(kukubirthday)
--- python oprator/activity3.py
+++ python oprator/activity3.py
@@ -4,4 +4,4 @@ marks3=int(input("enter your english marks-"))
 marks4=int(input("enter your science marks-"))
 marks5=int(input("enter your sst marks-"))
 percentage=((marks1+marks2+marks3+marks4+marks5)/500)*100
-print("percentage of all marks-",percentage)
+print("percentage of all marks-",percentage
